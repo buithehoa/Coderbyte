ed22dd89925fac4c83f4b17452ce03b88d8ff4d8
Bui The Hoa <buithehoa@gmail.com>
2024-06-01 17:50:27 +0700

    Add solution.

---
+++ CodelandUsernameValidation/codeland_username_validation.rb
@@ -0,0 +1,17 @@
+def CodelandUsernameValidation(str)
+  # code goes here
+  valid = "true"
+  invalid = "false"
+
+  return invalid if str.empty?
+
+  return invalid if str.length < 4 || str.length > 25
+
+  return invalid if str.match(/^[a-zA-Z]{1}\w+$(?<!_)/).nil?
+  
+  return valid
+end
+
+# keep this function call here 
+puts CodelandUsernameValidation(STDIN.gets)
+
